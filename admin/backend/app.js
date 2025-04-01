708c9855aede4113bccb8e6ef0ef81ce06a5abe8
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-04-01 11:32:49 +0530

    changes in backend/app.js and backend/routes/book.js

--- admin/backend/app.js
+++ admin/backend/app.js
@@ -3,10 +3,12 @@ const app = express()
 require("dotenv").config();
 require("./conn/conn");
 const User = require("./routes/user");
+const Books = require("./routes/book");
 
 app.use(express.json());
 //routes
 app.use("/api/v1", User);
+app.use("/api/v1", Books);
 //creating Port
 app.listen(process.env.PORT, () => {
     console.log(`Server Started at port ${process.env.PORT}`);
