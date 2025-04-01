e6c6c62497fd094f401053e3a4b7fd7927e990c3
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-04-01 12:33:27 +0530

    changes in routes/book.js - added get-book-by-id section

--- admin/backend/routes/book.js
+++ admin/backend/routes/book.js
@@ -78,4 +78,34 @@ router.get("/get-all-books",async (req, res) => {
     }
 });
 
+//get recently added books limit 4
+router.get("/get-recent-books", async(req,res) => {
+    try {
+        const books = await Book.find().sort({ createdAt : -1 }).limit(4);
+        return res.json({
+            status: "Success",
+            data: books,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "An error occurred" });
+    }
+});
+
+//get book by id
+router.get("/get-book-by-id/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        return res.json({
+            status: "Success",
+            data: book,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message : "An error occurred" });
+    }
+});
+
 module.exports = router;
