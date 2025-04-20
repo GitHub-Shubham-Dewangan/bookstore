7079f481290049a804e7294aa26cfdda1e14e961
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-04-20 14:14:58 +0530

    remove book section added in favourite.js file

--- admin/backend/routes/favourite.js
+++ admin/backend/routes/favourite.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const { authenticateToken } = require("./userAuth");
 
 // add book to favourite
-router.put("add-book-to-favourite", authenticateToken, async ( req, res) => {
+router.put("/add-book-to-favourite", authenticateToken, async ( req, res) => {
     try {
         const { bookid, id } = req.headers;
         const userData = await User.findById(id);
@@ -16,6 +16,22 @@ router.put("add-book-to-favourite", authenticateToken, async ( req, res) => {
         return res.status(200).json({ message: "Book added to favourites"});
     } catch (error) {
         res.status(500).json({ message: "Internal server error"});
+    }
+});
+
+// remove book from favourite
+router.delete("/remove-book-from-favourite", authenticateToken, async (req, res) => {
+    try {
+        const { bookid, id } = req.headers;
+        const userData = await User.findById(id);
+        const isBookFavourite = userData.favourites.includes(bookid);
+        if (isBookFavourite){
+            await User.findByIdAndUpdate(id, { $pull: { favourites: bookid }});
+        }
+
+        return res.status(200).json({ message: "Book removed from favourites"});
+    } catch (error){
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
