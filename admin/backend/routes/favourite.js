e97d63269ad5af1f7524c244e5a704ff8e9aee8c
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-04-20 14:29:44 +0530

    added get favourite books of a particular user section in favourites.js

--- admin/backend/routes/favourite.js
+++ admin/backend/routes/favourite.js
@@ -19,7 +19,7 @@ router.put("/add-book-to-favourite", authenticateToken, async ( req, res) => {
     }
 });
 
-// remove book from favourite
+//remove book from favourite
 router.delete("/remove-book-from-favourite", authenticateToken, async (req, res) => {
     try {
         const { bookid, id } = req.headers;
@@ -32,6 +32,22 @@ router.delete("/remove-book-from-favourite", authenticateToken, async (req, res)
         return res.status(200).json({ message: "Book removed from favourites"});
     } catch (error){
         res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+//get Favourite books of a particular user
+router.get("/get-favourite-books", authenticateToken, async (req, res) => {
+    try {
+        const { id } = req.headers;
+        const userData = await User.findById(id).populate("favourites");
+        const favouriteBooks = userData.favourites;
+        return res.json({
+            status:"Success",
+            data: favouriteBooks,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "An error occurred" });
     }
 });
 
