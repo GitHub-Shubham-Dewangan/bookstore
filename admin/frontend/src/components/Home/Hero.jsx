b6368d388177f75e064d69a77766e34bbaaa2910
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-05-01 13:24:21 +0530

    modified files

--- admin/frontend/src/components/Home/Hero.jsx
+++ admin/frontend/src/components/Home/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Home = () => {
-    return <div className="bg-zinc-900 text-white"></div>
+    return (
+    <div className="h-[75vh] flex">
+        <div className="w-3/6">
+        <h1 className="text-6xl font-semibold text-yellow-100">Where Every Page Begins a New Adventure</h1>
+        <p className="mt-4 text-xl text-zinc-300">Discover a world of books at KitaabGhar. From fresh releases to timeless reads, we bring books that spark curiosity, fuel passion, and stay with you long after the last page.</p>
+        </div>
+        <div className="w-3/6"></div>
+    </div>
+    );
 };
 
 export default Home;
