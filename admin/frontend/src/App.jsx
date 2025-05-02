2adc49ff2a549680e5f947b19651953eceefcdce
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-05-02 16:06:14 +0530

    perfect till 03:00 Hrs

--- admin/frontend/src/App.jsx
+++ admin/frontend/src/App.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AllBooks from "./pages/AllBooks";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/all-books" element={<AllBooks />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/profile" element={<Profile />} />
           <Route exact path="/SignUp" element={<SignUp />} />
           <Route exact path="/LogIn" element={<LogIn />} />
         </Routes>
