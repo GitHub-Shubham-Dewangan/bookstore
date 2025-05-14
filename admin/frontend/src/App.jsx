8ba5041e50a10084e89721dce0d65057ab891627
Shubham Dewangan <shubhamdewangan102@gmail.com>
2025-05-15 01:28:53 +0530

    completed till 04:45 Hrs

--- admin/frontend/src/App.jsx
+++ admin/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AllBooks from "./pages/AllBooks";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
@@ -13,19 +13,17 @@ import ViewBookDetails from "./components/ViewBookDetails/ViewBookDetails";
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/all-books" element={<AllBooks />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/SignUp" element={<SignUp />} />
-          <Route exact path="/LogIn" element={<LogIn />} />
-          <Route path="view-book-details/:id" element={<ViewBookDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/all-books" element={<AllBooks />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/SignUp" element={<SignUp />} />
+        <Route exact path="/LogIn" element={<LogIn />} />
+        <Route path="view-book-details/:id" element={<ViewBookDetails />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
--- admin/frontend/src/pages/LogIn.jsx
+++ admin/frontend/src/pages/LogIn.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  const [Values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const change = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...Values, [name]: value });
+  };
+
+  const submit = async () => {
+    try {
+      if (Values.username === "" || Values.password === "") {
+        alert("All fields are required");
+      } else {
+        const response = await axios.post(
+          "http://localhost:3000/api/v1/sign-in",
+          Values,
+        );
+        console.log(response.data);
+        //navigate("/LogIn");
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="h-auto bg-zinc-900 px-12 py-8 flex items-center justify-center">
+      <div className="bg-zinc-800 rounded-lg px-8 py-5 w-full md:w-3/6 lg:2-2/6">
+        <p className="text-zinc-200 text-xl">Log In</p>
+        <div className="mt-4">
+          <div>
+            <label htmlFor="" className="text-zinc-400">
+              Username
+            </label>
+            <input
+              type="text"
+              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none"
+              placeholder="Enter Username"
+              name="username"
+              required
+              value={Values.username}
+              onChange={change}
+            />
+          </div>
+          <div className="mt-4">
+            <label htmlFor="" className="text-zinc-400">
+              Password
+            </label>
+            <input
+              type="password"
+              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-2 outline-none"
+              placeholder="Enter password"
+              name="password"
+              required
+              value={Values.password}
+              onChange={change}
+            />
+          </div>
+          <div className="mt-4">
+            <button
+              className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+              onClick={submit}
+            >
+              Login
+            </button>
+          </div>
+          <p className="flex mt-4 items-center justify-center text-zinc-200 font-semibold">
+            Or
+          </p>
+          <p className="flex mt-4 items-center justify-center text-zinc-500 font-semibold">
+            Don't have an account ? &nbsp;
+            <Link to="/signup" className="hover:text-blue-500">
+              <u>SignUp</u>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
